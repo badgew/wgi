13651a75797a324f0d3b27bb47a016012b4cb6c5
Whitney Badge <whitney.badge@gmail.com>
2018-10-31 15:09:26 -0700

    files

---
+++ scripts.js
@@ -0,0 +1,59 @@
+
+
+
+
+(function($) {
+   $(document).ready(function(){
+
+   	 var months = $('.month-input');
+   	 var closeMonths = function(currentMonth) {
+   	 	months.each(function(i, e) {
+   	 		if(currentMonth != e) {
+   	 			$( e ).prop( "checked", false );
+   	 		}
+   	 	});
+   	 }
+   	 
+   	 months.click(function(e) {
+   	 	closeMonths(this);
+   	 });
+
+   	 $(".blog-plus").click(function(e){
+   	 	$(".right").toggleClass("active");
+   	 });
+
+   	 // timezones
+
+	var relativeTime = function(){
+		currentTime = moment(new Date());
+		relativeTime = moment.tz("US/Pacific").format('hh:mm a');
+		$(".relative-time").html("It's " + relativeTime + " PST<br>in California");
+	}
+	relativeTime();
+	window.setInterval(relativeTime, 10000);
+
+  // end timezone
+
+  // blog scroll
+
+	$(".october").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".blog-october").offset().top-80},
+	        'slow');
+	});
+
+	$(".november").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".blog-november").offset().top-80},
+	        'slow');
+	});
+
+	$(".december").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".blog-december").offset().top-80},
+	        'slow');
+	});
+
+	// end blog scroll
+   });
+})(jQuery);
